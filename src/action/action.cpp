c618efda5f8590a659e5b4adf77c57d8e81e3594
Joey Gouly <joey.gouly@gmail.com>
2016-06-18 12:16:17 +0100

    Fix off-by-one error in TOGGLE_LAYER.
    Layer 0 is currently 'implicit', so bit 0 in activeLayers should represent
    layer 1. Use MSB to see which is the highest active layer.

--- src/action/action.cpp
+++ src/action/action.cpp
@@ -1,6 +1,8 @@
 #include "action/action.h"
 #include "action/keycode.h"
 
+int msb(unsigned int v) { return v == 0 ? 0 : 32 - __builtin_clz(v); }
+
 void processKeys(const ResultT &result, const MapT &map, USBBuffer &buf,
                  const ResultT &prevResult, unsigned char &activeLayers) {
   for (unsigned C = 0; C < result.getNumColumns(); ++C) {
@@ -18,16 +20,16 @@ void processKeys(const ResultT &result, const MapT &map, USBBuffer &buf,
         }
       }
       if (state != KeyState::NONE) {
-        unsigned action = map[activeLayers].get(C, R);
+        unsigned action = map[msb(activeLayers)].get(C, R);
         if (IS_KEY(action)) {
           buf.push_back(GET_KEY(action));
         } else if (IS_MOD(action)) {
           buf.push_mod(GET_MOD(action));
         } else if (IS_LAYER(action)) {
           if (state == KeyState::PRESSED)
-            activeLayers |= (1 << GET_LAYER(action));
+            activeLayers |= (1 << (GET_LAYER(action) - 1));
           else if (state == KeyState::RELEASED)
-            activeLayers &= ~(1 << GET_LAYER(action));
+            activeLayers &= ~(1 << (GET_LAYER(action) - 1));
         }
       }
     }
