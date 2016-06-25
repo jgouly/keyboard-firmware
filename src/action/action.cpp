65433ba8e521b452495258044eebc6d50d3597bf
Joey Gouly <joey.gouly@gmail.com>
2016-06-25 17:29:33 +0100

    Split processKeys into smaller functions.

--- src/action/action.cpp
+++ src/action/action.cpp
@@ -5,6 +5,22 @@ static unsigned msb(unsigned int v) {
   return v == 0 ? 0 : 32 - __builtin_clz(v);
 }
 
+static void handleKey(USBBuffer &buf, unsigned action) {
+  buf.push_back(GET_KEY(action));
+}
+
+static void handleMod(USBBuffer &buf, unsigned action) {
+  buf.push_mod(GET_MOD(action));
+}
+
+static void handleLayer(unsigned action, const KeyState state,
+                        unsigned char &activeLayers) {
+  if (state == KeyState::PRESSED)
+    activeLayers |= (1 << (GET_LAYER(action) - 1));
+  else if (state == KeyState::RELEASED)
+    activeLayers &= ~(1 << (GET_LAYER(action) - 1));
+}
+
 void processKeys(const ResultT &result, const MapT &map, USBBuffer &buf,
                  const ResultT &prevResult, unsigned char &activeLayers) {
   for (unsigned C = 0; C < result.getNumColumns(); ++C) {
@@ -24,14 +40,11 @@ void processKeys(const ResultT &result, const MapT &map, USBBuffer &buf,
       if (state != KeyState::NONE) {
         unsigned action = map[msb(activeLayers)].get(C, R);
         if (IS_KEY(action)) {
-          buf.push_back(GET_KEY(action));
+          handleKey(buf, action);
         } else if (IS_MOD(action)) {
-          buf.push_mod(GET_MOD(action));
+          handleMod(buf, action);
         } else if (IS_LAYER(action)) {
-          if (state == KeyState::PRESSED)
-            activeLayers |= (1 << (GET_LAYER(action) - 1));
-          else if (state == KeyState::RELEASED)
-            activeLayers &= ~(1 << (GET_LAYER(action) - 1));
+          handleLayer(action, state, activeLayers);
         }
       }
     }
